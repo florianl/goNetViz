fe6c249e217f5cef2ec2e97c4e1be770ef4ec593
Lehner Florian <dev@der-flo.net>
2017-10-21 19:01:08 +0200

    Fix terminal output bug
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- goNetViz.go
+++ goNetViz.go
@@ -80,6 +80,8 @@ func createTerminalVisualization(pkt1 Data, pkt2 Data, bitsPerPixel uint) {
 	var bitPos, cpyBitPos int
 	var bytePos, cpyBytePos int
 	var pkt1Len, pkt2Len int
+	var r1, g1, b1 uint8
+	var r2, g2, b2 uint8
 
 	pkt1Len = len(pkt1.payload)
 	pkt2Len = len(pkt2.payload)
@@ -87,18 +89,18 @@ func createTerminalVisualization(pkt1 Data, pkt2 Data, bitsPerPixel uint) {
 	bytePos = 0
 	for {
 		if bytePos >= pkt1Len {
-			r2, g2, b2 := createPixel(pkt2.payload, &bytePos, &bitPos, bitsPerPixel)
-			fmt.Printf("\x1B[38;2;%d;%d;%dm\u2580", uint8(r2), uint8(g2), uint8(b2))
+			r1, g1, b1 = 0x00, 0x00, 0x00
+			r2, g2, b2 = createPixel(pkt2.payload, &bytePos, &bitPos, bitsPerPixel)
 		} else if bytePos >= pkt2Len {
-			r1, g1, b1 := createPixel(pkt1.payload, &bytePos, &bitPos, bitsPerPixel)
-			fmt.Printf("\x1B[38;2;%d;%d;%dm\u2580", uint8(r1), uint8(g1), uint8(b1))
+			r1, g1, b1 = createPixel(pkt1.payload, &bytePos, &bitPos, bitsPerPixel)
+			r2, g2, b2 = 0x00, 0x00, 0x00
 		} else {
 			cpyBitPos = bitPos
 			cpyBytePos = bytePos
-			r1, g1, b1 := createPixel(pkt1.payload, &cpyBytePos, &cpyBitPos, bitsPerPixel)
-			r2, g2, b2 := createPixel(pkt2.payload, &bytePos, &bitPos, bitsPerPixel)
-			fmt.Printf("\x1B[48;2;%d;%d;%dm\x1B[38;2;%d;%d;%dm\u2580", uint8(r2), uint8(g2), uint8(b2), uint8(r1), uint8(g1), uint8(b1))
+			r1, g1, b1 = createPixel(pkt1.payload, &cpyBytePos, &cpyBitPos, bitsPerPixel)
+			r2, g2, b2 = createPixel(pkt2.payload, &bytePos, &bitPos, bitsPerPixel)
 		}
+		fmt.Printf("\x1B[48;2;%d;%d;%dm\x1B[38;2;%d;%d;%dm\u2580", r2, g2, b2, r1, g1, b1)
 		if bytePos >= pkt1Len && bytePos >= pkt2Len {
 			break
 		}
