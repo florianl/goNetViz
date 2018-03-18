f8c4ea0a3123ab0b0d189e83fc7289850a478555
Lehner Florian <dev@der-flo.net>
2018-03-18 20:10:37 +0100

    Add Socket as source
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- goNetViz.go
+++ goNetViz.go
@@ -311,7 +311,13 @@ func initSource(input, filter string, pcap bool) (handle Source, err error) {
 		f.file, err = os.Open(input)
 		handle = f
 	case mode&os.ModeCharDevice == 0:
-		fallthrough
+		f := new(regularFile)
+		f.file, err = os.Open(input)
+		handle = f
+	case mode & os.ModeSocket == 0:
+		f := new(regularFile)
+		f.file, err = os.Open(input)
+		handle = f
 	default:
 		return nil, fmt.Errorf(fmt.Sprintf("Can not handle %s as source", input))
 	}
